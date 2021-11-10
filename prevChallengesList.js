22a2476162bc7ab024acafdaa3e62bebbd64ca26
Mahmoud Samy Mohamed Mahmoud <mahmoudsamy50@outlook.com>
2021-11-10 19:29:30 +0200

    reversed the previous challenges list

--- prevChallengesList.js
+++ prevChallengesList.js
@@ -1,19 +1,19 @@
 export async function renderPreviousChallenges() {
-  const response = await fetch('./prev_challenges/challenges.json')
-  const challenges = await response.json()
+  const response = await fetch("./prev_challenges/challenges.json");
+  const challenges = await response.json();
 
-  const container = document.querySelector('.prev_challenges')
-  const unorderedList = document.createElement('ul')
+  const container = document.querySelector(".prev_challenges");
+  const unorderedList = document.createElement("ul");
 
-  challenges.challenges.forEach((challenge) => {
-    const listItem = document.createElement('li')
-    const anchorTag = document.createElement('a')
-    anchorTag.textContent = challenge.name
-    anchorTag.setAttribute('href', challenge.path)
+  challenges.challenges.reverse().forEach((challenge) => {
+    const listItem = document.createElement("li");
+    const anchorTag = document.createElement("a");
+    anchorTag.textContent = challenge.name;
+    anchorTag.setAttribute("href", challenge.path);
 
-    listItem.appendChild(anchorTag)
-    unorderedList.appendChild(listItem)
-  })
+    listItem.appendChild(anchorTag);
+    unorderedList.appendChild(listItem);
+  });
 
-  container.insertAdjacentElement('beforeend', unorderedList)
+  container.insertAdjacentElement("beforeend", unorderedList);
 }
